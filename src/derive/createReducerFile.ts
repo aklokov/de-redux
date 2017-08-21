bdf11cfad975f947acea52c574b4c1e1ec952e31
repinvv <repin.vladv@gmail.com>
2017-08-22 00:40:09 +0300

    creating tree wip

---
+++ src/derive/createReducerFile.ts
@@ -0,0 +1,6 @@
+import { ReducerFile, ChildReducer } from './model';
+import { State, Reduction } from '../parse/model';
+
+export function createReducerFile(state: State, reductions: Reduction[]): void {
+
+}
--- src/derive/deriveModel.ts
+++ src/derive/deriveModel.ts
@@ -2,13 +2,15 @@ import { Model } from './model';
 import { Model as InputModel, Reduction } from '../parse/model';
 import { Options } from '../Options';
 import * as _ from 'lodash';
-import { createActionFile } from '.';
+import { createActionFile, createReducerFile } from '.';
 import { toStringLookup } from 'hash-map';
-export function deriveModel(options: Options, input: InputModel): Model {
-  const reductionMap = toStringLookup(input.reductions, red => red.stateId);
-  const actionFiles = input.states
-    .map(state => createActionFile(state, reductionMap[state.id]));
+import { createTree } from './tree';
 
+export function deriveModel(options: Options, input: InputModel): Model {
+  const tree = createTree(input.states);
+  const reductionMap = toStringLookup(input.reductions, red => red.stateId);
+  const actionFiles = input.states.map(state => createActionFile(state, reductionMap[state.id]));
+  const reducers = input.states.map(state => createReducerFile(state, reductionMap[state.id]));
   return {
     actionFiles,
     reducerFiles: [],
